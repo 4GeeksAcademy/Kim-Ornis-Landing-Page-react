7d0c7fb19eca9aa39687b074be07d955b4cad0ae
KOTHECODE <160642762+KOTHECODE@users.noreply.github.com>
2024-05-16 01:06:50 +0000

    still pending components

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -7,79 +7,78 @@ import rigoImage from "../../img/rigo-baby.jpg";
 const Home = () => {
 	return (
 	<>	
-	<nav class="navbar fixed-top bg-body-tertiary">
-  		<div class="container-fluid">
-    		<a class="navbar-brand" href="#">Start Bootstrap</a>
+	<nav className="navbar fixed-top bg-body-tertiary py-0">
+  		<div className="container-fluid">
+    		<a className="navbar-brand" href="#">Start Bootstrap</a>
 			<div className="nav-links">
-			<a class="navbar-brand" href="#">Home</a>
-			<a class="navbar-brand" href="#">About</a>
-			<a class="navbar-brand" href="#">Services</a>
-			<a class="navbar-brand" href="#">Contact</a>
+			<a className="navbar-brand" href="#">Home</a>
+			<a className="navbar-brand" href="#">About</a>
+			<a className="navbar-brand" href="#">Services</a>
+			<a className="navbar-brand" href="#">Contact</a>
 			</div>
   		</div>
 	</nav><br></br>
 	<div className="container">
-	<div class="container text-center">
-		<div class="row">
-			<div class="col">
-			<div class="jumbotron">
-				<h1 class="display-4">A Warm Welcome!</h1>
-				<p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
-				<hr class="my-4"></hr>
-				<p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
-				<p class="lead">
-					<a class="btn btn-primary btn-lg" href="#" role="button">Call to Action</a>
+	<div className="container text-center">
+		<div className="row">
+			<div className="col">
+			<div className="jumbotron">
+				<h1 className="display-4">A Warm Welcome!</h1>
+				<p className="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.
+				It uses utility classes for typography and spacing to space content out within the larger container.</p>
+				<p className="lead">
+					<a className="btn btn-primary btn-lg" id="call-to" href="#" role="button">Call to Action</a>
 				</p>
 			</div>
 			</div>
 		</div><br></br>
-		<div class="row">
-			<div class="col">
-				<div class="card" style= {{width:'18rem'}}>
-					<img src="img/nando.jpeg" class="card-img-top" alt="..."/>
-					<div class="card-body">
-						<h5 class="card-title">Card title</h5>
-						<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-						<a href="#" class="btn btn-primary">Find Our More!</a>
+		<div className="row">
+			<div className="col">
+				<div className="card" style= {{width:'18rem'}}>
+					<img src="https://www.w3schools.com/images/w3schools_green.jpg" class="card-img-top" alt="..."/>
+					<div className="card-body">
+						<h5 className="card-title">Card title</h5>
+						<p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+						<a href="#" className="btn btn-primary">Find Our More!</a>
 					</div>
 				</div>
 			</div>
-			<div class="col">
-				<div class="card" style= {{width:'18rem'}}>
-						<img src="..." class="card-img-top" alt="..."/>
-						<div class="card-body">
-							<h5 class="card-title">Card title</h5>
-							<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+			<div className="col">
+				<div className="card" style= {{width:'18rem'}}>
+						<img src="https://www.w3schools.com/images/w3schools_green.jpg" class="card-img-top" alt="..."/>
+						<div className="card-body">
+							<h5 className="card-title">Card title</h5>
+							<p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
 							<a href="#" class="btn btn-primary">Find Our More!</a>
 						</div>
 				</div>
 			</div>
-			<div class="col">
-				<div class="card" style= {{width:'18rem'}}>
-					<img src="..." class="card-img-top" alt="..."/>
-					<div class="card-body">
-						<h5 class="card-title">Card title</h5>
-						<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-						<a href="#" class="btn btn-primary">Find Our More!</a>
+			<div claclassNamess="col">
+				<div className="card" style= {{width:'18rem'}}>
+					<img src="https://www.w3schools.com/images/w3schools_green.jpg" class="card-img-top" alt="..."/>
+					<div className="card-body">
+						<h5 className="card-title">Card title</h5>
+						<p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+						<a href="#" className="btn btn-primary">Find Our More!</a>
 					</div>
 				</div>
 			</div>
-			<div class="col">
-				<div class="card" style= {{width:'18rem'}}>
-					<img src="..." class="card-img-top" alt="..."/>
-					<div class="card-body">
-						<h5 class="card-title">Card title</h5>
-						<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-						<a href="#" class="btn btn-primary">Find Our More!</a>
+			<div className="col">
+				<div className="card" style= {{width:'18rem'}}>
+					<img src="https://www.w3schools.com/images/w3schools_green.jpg" class="card-img-top" alt="..."/>
+					<div className="card-body">
+						<h5 className="card-title">Card title</h5>
+						<p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+						<a href="#" className="btn btn-primary">Find Our More!</a>
 					</div>
 				</div>
 			</div>
 		</div>
 		</div>
 	</div>
-	<nav class="navbar sticky-bottom bg-body-tertiary">
-  		<div class="container-fluid">
-    	<a class="navbar-brand" href="#">Copyright @ Your Website 2024</a>
+	<nav className="navbar sticky-bottom bg-body-tertiary py-0">
+  		<div className="container-fluid mt-3">
+    	<a className="navbar-brand" href="#">Copyright @ Your Website 2024</a>
   	</div>
 </nav>
 	</>
